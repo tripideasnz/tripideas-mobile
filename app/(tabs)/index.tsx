a3a49773621caad2b8021aee875c7d7258f0117f
Douglas Paul <douglas@tripideas.nz>
2026-05-30 17:26:17 +1200

    Added TripIdeas home screen text

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -22,6 +22,12 @@ export default function HomeScreen() {
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
+        <ThemedText type="subtitle">TripIdeas</ThemedText>
+        <ThemedText>
+          Discover places, build collections and create itineraries.
+        </ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
